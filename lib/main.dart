45105b4eba27c71976461432e5ef1ab0cfa1bae6
dabuzida <dabuzida@gmail.com>
2022-05-25 14:40:02 +0900

    220525

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RenderBox',
+      title: 'Image',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('RenderBox'),
+          title: const Text('Image'),
           elevation: 0,
           foregroundColor: Colors.cyanAccent,
           backgroundColor: Colors.black,
